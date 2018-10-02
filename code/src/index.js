cd5725f4e2684505f7a8773559feb409e5394974
Naomi <naomi.tesfay@gmail.com>
2018-10-02 15:09:18 +0200

    assignment-clock

--- code/src/index.js
+++ code/src/index.js
@@ -3,16 +3,57 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date()
+    }
+  }
+
+componentDidMount() {
+    this.startTimer ()
+  }
+
+componentWillUnmount() {
+  this.stopTimer()
+}
+
+stopTimer = () => {
+  clearInterval(this.timerID)
+}
+
+startTimer = () => {
+  this.timerID = setInterval(
+    () => this.tick(),
+    1000
+  )
+}
+
+tick() {
+  this.setState({
+      date: new Date()
+
+  })
+
+}
+
+
 
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      <div className="container">
+        <h1>What time is it?</h1>
+        <h2>{this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={this.stopTimer}>Stop</button>
+        <button onClick={this.startTimer}>Start</button>
+        </div>
       </div>
-    )
+    );
   }
-
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
